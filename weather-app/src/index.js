b32e616082aa7aba7e5be4ec4cec399c33563227
Silongo Vailolo <silongo68@hotmail.com>
2025-07-15 18:47:54 +1000

    selected elements using query selector for for use and also used event listener to to wait until my html file is fully loaded

---
+++ weather-app/src/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.querySelector('#city-input');
+    const switcher = document.querySelector('.switcher');
+    const searchBtn = document.querySelector('#search-btn');
+    const weatherDiv = document.querySelector('#weather-result');
+    const useLiveToggle = document.querySelector('#live-toggle');
+
+    const apiKey = 'a2ad7a5fb4efcbb8bd83381b0f2806a3';
+});
